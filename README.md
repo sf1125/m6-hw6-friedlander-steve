0088c8e507eaffe4c57d9855d466b03bcb15e5db
Steve Friedlander <s.friedlander@ufl.edu>
2021-06-17 15:46:41 -0400

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # m6-hw6-friedlander-steve
+
+I had attempted to utilize various resources suck at stackoverflow and youtube
+to complete the assignment. I do not have the various URLs. Prof. Ram worked with me
+one-on-one to help understand the Javascript concepts.
--- main.js
+++ main.js
@@ -1,41 +1,98 @@
 // Change all box colors on mouseover
 
-var box-color = document.getElementsByClassName('box1', 'box2', 'box3');
+var boxes = document.getElementsByClassName('square');
+var box1 = boxes[0];
+var box2 = boxes[1];
+var box3 = boxes[2];
 
-function colorChange(){
-    box-color.style.color = '#FF0000';
-};
-
-for (i=0; i<colors.length; i++) {
-    colors[i].addEventListener('mouseover', colorChange);
+function changeColor() {
+  for (i=0; i<boxes.length; i++){
+    boxes[i].classList.toggle('redcolor');
+  }
 }
+for (i=0; i<boxes.length; i++) {
+  boxes[i].addEventListener('mouseenter', changeColor) 
+  boxes[i].addEventListener('mouseleave', changeColor) 
+}
+
+
+//Shows hidden button
+
+var button = document.getElementsByClassName('start_over')[0];
+
+function showButton () {
+  button.classList.remove('hidden');
+}
+
+box1.addEventListener('click', showButton);
+box2.addEventListener('click', showButton);
+box3.addEventListener('click', showButton);
 
 // Box 1 - Close, but no cigar message
 
-var myElement1 = document.createTextNode('click',
-function(event) {
-  var message1 = document.getElementByClassName('box1');
-  message1.innerHTML = 'Oooh - so close, but no cigar';
-  },
-);
+var message = document.getElementsByClassName('message')[0];
+function box1Message () {
+  message.innerHTML='Oooh - so close, but no cigar';
+}
+
+box1.addEventListener('click', box1Message);
+
 
 // Box 2 - DING DING DING - We have a winner message
 
-var myElement2 = document.createTextNode('click',
-function(event) {
-  var message2 = document.getElementByClassName('box2');
-  message1.innerHTML = 'DING DING DING - We have a winner';
-  },
-);
+var message = document.getElementsByClassName('message')[0];
+function box2Message () {
+  message.innerHTML='DING DING DING - We have a winner';
+}
+
+box2.addEventListener('click', box2Message);
+
 
 // Box 3 - Oops, butter luck next time message
 
-var myElement3 = document.createTextNode('click',
-function(event) {
-  var message3 = document.getElementByClassName('box3');
-  message1.innerHTML = 'Oops, butter luck next time';
-  },
-);
+var message = document.getElementsByClassName('message')[0];
+function box3Message () {
+  message.innerHTML='Oops, butter luck next time';
+}
+
+box3.addEventListener('click', box3Message);
+
+
+
+// function colorChange(){
+//     boxes.style.color = '#FF0000';
+// };
+
+// for (i=0; i<boxes.length; i++) {
+//     boxes[i].addEventListener('mouseenter', colorChange);
+// }
+
+// Box 1 - Close, but no cigar message
+
+// var myElement1 = document.createTextNode('onclick',
+// function(event) {
+//   var message1 = document.getElementByClassName('box1');
+//   message1.innerText = 'Oooh - so close, but no cigar';
+//   },
+// );
+
+// // Box 2 - DING DING DING - We have a winner message
+
+// var myElement2 = document.createTextNode('onclick',
+// function(event) {
+//   var message2 = document.getElementByClassName('box2');
+//   message1.innerText = 'DING DING DING - We have a winner';
+//   },
+// );
+
+// // Box 3 - Oops, butter luck next time message
+
+// var myElement3 = document.createTextNode('onclick',
+// function(event) {
+//   var message3 = document.getElementByClassName('box3');
+//   message1.innerText = 'Oops, butter luck next time';
+//   },
+// );
 
 
 
